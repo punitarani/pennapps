da6811ee5b4f6945912dc2fcbcb9ac20dcbcb02c
Punit Arani <punitsai36@gmail.com>
2023-09-08 17:29:47 -0700

    Configure CORS

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ FastAPI application entry point.
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
