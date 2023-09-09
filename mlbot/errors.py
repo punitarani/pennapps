4b68c259f723e73e286721043a4981dd4564b0e4
Punit Arani <punitsai36@gmail.com>
2023-09-08 19:18:20 -0700

    Add DataNotAvailable error
    Required by c244433fc6334a9a8dc352d6b95880c7b8ba8260

---
+++ mlbot/errors.py
@@ -0,0 +1,20 @@
+"""mlbot.errors.py"""
+
+from uuid import UUID
+
+
+class DataNotAvailable(Exception):
+    """Data not available error"""
+
+    def __init__(self, user_id: str, file_id: str, file_type: str = "csv") -> None:
+        """
+        Initialize DataNotAvailable exception.
+
+        Args:
+            user_id (UUID): User ID
+            file_id (UUID): File ID
+        """
+
+        super().__init__(
+            f"Data not available for user_id={user_id}, file_id={file_id}, file_type={file_type}"
+        )
