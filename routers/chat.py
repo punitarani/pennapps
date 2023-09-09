e7fe20427de424f8ab7c5e55dc3ebdb4101c73b9
Punit Arani <punitsai36@gmail.com>
2023-09-08 19:14:24 -0700

    Create get_response() endpoint

--- routers/chat.py
+++ routers/chat.py
@@ -1,7 +1,28 @@
 """routers.chat.py"""
 
-from fastapi import FastAPI, APIRouter
+from uuid import UUID
+
+from fastapi import FastAPI, APIRouter, Body, Depends
+from pydantic import BaseModel
+
+from dependencies import get_user_id
 
 app = FastAPI()
 
 router = APIRouter(prefix="/chat", tags=["chat"])
+
+
+class ChatRequest(BaseModel):
+    file_id: UUID
+    file_type: str
+    query: str
+
+
+@router.post("/")
+async def get_response(
+    request: ChatRequest = Body(...),
+    user_id: UUID = Depends(get_user_id),
+):
+    return {
+        "answer": request.query,
+    }
