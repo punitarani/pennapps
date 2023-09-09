c58c339b09e6ccd7077870d54c0a7d79a86f33bb
Punit Arani <punitsai36@gmail.com>
2023-09-08 20:34:50 -0700

    [Major] Create basic code interpreter

--- mlbot/mlbot.py
+++ mlbot/mlbot.py
@@ -1,13 +1,21 @@
 """mlbot.mlbot.py"""
 
+import json
+from copy import deepcopy
 from pathlib import Path
 from uuid import UUID
 
+import openai
 import pandas as pd
 
 from config import DATA_DIR
 from mlbot.errors import DataNotAvailable
 from mlbot.models import FileType
+from mlbot.utils import load_prompts, load_prompt_messages, load_prompt_functions
+
+PROMPTS = load_prompts()
+PROMPT_MESSAGES = load_prompt_messages()
+PROMPT_FUNCTIONS = load_prompt_functions()
 
 
 class MLBot:
@@ -39,7 +47,7 @@ class MLBot:
             str: CSV file path
         """
 
-        return DATA_DIR.joinpath(self.user_id, f"{self.file_id}.parquet")
+        return DATA_DIR.joinpath("users", self.user_id, f"{self.file_id}.parquet")
 
     def load_file(self) -> pd.DataFrame:
         """
@@ -55,3 +63,79 @@ class MLBot:
             raise DataNotAvailable(
                 user_id=self.user_id, file_id=self.file_id, file_type=self.file_type
             )
+
+    def query_file(self, query: str) -> str:
+        """
+        Query file.
+
+        Args:
+            query (str): Query
+
+        Returns:
+            str: Query result
+        """
+
+        df = deepcopy(self.data)
+
+        base_prompt = PROMPTS["MLBot.query_file"]
+        messages = [{"role": "user", "content": df.describe().to_string()}]
+        functions = PROMPT_FUNCTIONS["MLBot.task_to_python"]
+
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a smart Data Scientist tasked with writing efficient"
+                    " and accurate python and pandas queries to solve the following task.",
+                },
+                {
+                    "role": "user",
+                    "content": base_prompt.format(task=query),
+                },
+                *messages,
+            ],
+            temperature=0.4,
+            max_tokens=256,
+            functions=functions,
+            function_call={"name": "task_to_python"},
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+
+        python_code = json.loads(
+            response["choices"][0]["message"]["function_call"]["arguments"]
+        )["python"]
+        kwargs = {"df": df}
+
+        result = self.execute_py(code=python_code, **kwargs)
+
+        return result
+
+    @staticmethod
+    def execute_py(code, **kwargs) -> str:
+        """
+        Execute Python code.
+
+        Args:
+            code (str): Python code to be executed
+            **kwargs: Arbitrary keyword arguments
+
+        Returns:
+            str: Execution result
+        """
+
+        local_vars = {**kwargs}
+        try:
+            # Attempt to evaluate the code as an expression
+            result = eval(code, {}, local_vars)
+        except SyntaxError:
+            # If the code is not an expression (i.e., it's a statement),
+            # then execute it and fetch the 'result' variable if it exists
+            exec(code, {}, local_vars)
+            result = local_vars.get("result", "Execution completed successfully")
+        except Exception as e:
+            result = str(e)
+
+        return result
---
+++ mlbot/utils.py
@@ -0,0 +1,77 @@
+"""mlbot.utils.py"""
+
+import json
+from pathlib import Path
+from types import MappingProxyType
+
+
+def load_prompts() -> MappingProxyType[str, str]:
+    """
+    Load the prompts from the prompts.json file.
+
+    Returns:
+        A dictionary of prompts.
+        It is a mapping proxy type, so it is immutable.
+    """
+
+    prompts_dir = Path(__file__).parent.joinpath("prompts")
+
+    prompt_files = ["MLBot.query_file"]
+
+    # Load the prompts from each file
+    prompts = {}
+    for prompt_file in prompt_files:
+        with open(prompts_dir.joinpath(prompt_file), "r") as f:
+            prompts[prompt_file] = f.read()
+
+    return MappingProxyType(prompts)
+
+
+def load_prompt_functions() -> MappingProxyType[str, str]:
+    """
+    Load the prompt functions from the functions.json file.
+
+    Returns:
+        A dictionary of prompt functions.
+        It is a mapping proxy type, so it is immutable.
+    """
+
+    prompts_dir = Path(__file__).parent.joinpath("prompts")
+    prompt_functions_fp = prompts_dir.joinpath("functions.json")
+
+    functions = ["MLBot.task_to_python"]
+
+    with open(prompt_functions_fp, "r") as f:
+        prompt_functions = f.read()
+
+    # Ensure all functions are in the functions.json file
+    for function in functions:
+        assert (
+            function in prompt_functions
+        ), f"Function {function} not in functions.json"
+
+    return MappingProxyType(json.loads(prompt_functions))
+
+
+def load_prompt_messages() -> MappingProxyType[str, str]:
+    """
+    Load the prompt messages from the messages.json file.
+
+    Returns:
+        A dictionary of prompt messages.
+        It is a mapping proxy type, so it is immutable.
+    """
+
+    prompts_dir = Path(__file__).parent.joinpath("prompts")
+    prompt_messages_fp = prompts_dir.joinpath("messages.json")
+
+    messages = []
+
+    with open(prompt_messages_fp, "r") as f:
+        prompt_messages = f.read()
+
+    # Ensure all messages are in the messages.json file
+    for message in messages:
+        assert message in prompt_messages, f"Message {message} not in messages.json"
+
+    return MappingProxyType(json.loads(prompt_messages))
