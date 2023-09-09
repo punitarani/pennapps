caa43c24d24c44c21c230477c71d5ed0e61c1a1b
Punit Arani <punitsai36@gmail.com>
2023-09-08 19:35:40 -0700

    Configure openai

--- mlbot/__init__.py
+++ mlbot/__init__.py
@@ -4,8 +4,15 @@ mlbot.__init__.py
 mlbot package
 """
 
+import os
+
+import openai
 from dotenv import load_dotenv
 
 from config import PROJECT_DIR
 
 load_dotenv(PROJECT_DIR.joinpath(".env"))
+
+# Configure OpenAI API key
+assert os.getenv("OPENAI_API_KEY"), "OPENAI_API_KEY environment variable not set"
+openai.api_key = os.getenv("OPENAI_API_KEY")
