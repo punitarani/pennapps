5b2b0bf550e4caf330c0ab33775bf68e8919675a
Punit Arani <punitsai36@gmail.com>
2023-09-08 17:27:06 -0700

    Add uvicorn main guard

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+main.py
+
+FastAPI application entry point.
+"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +17,15 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", default=8000)),
+        log_level="info",
+    )
