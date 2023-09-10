65691d5193b29eb8de7dcf099fc37b054bac14dc
Punit Arani <punitsai36@gmail.com>
2023-09-09 23:26:24 -0700

    Add analyze_ml_models() and /analyze/models endpoint

--- mlbot/agent.py
+++ mlbot/agent.py
@@ -1,15 +1,15 @@
 """mlbot.agent.py"""
 
 from codeinterpreterapi import CodeInterpreterSession, File
+from codeinterpreterapi.schema import CodeInterpreterResponse
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
 
 from config import PROJECT_DIR
 
 load_dotenv(PROJECT_DIR.joinpath(".env"))
 
 
-async def analyze_file(filepath: str) -> tuple[str, list[File]]:
+async def analyze_file(filepath: str) -> CodeInterpreterResponse:
     async with CodeInterpreterSession() as session:
         # define the user request
         user_request = (
@@ -18,6 +18,7 @@ async def analyze_file(filepath: str) -> tuple[str, list[File]]:
             "The analysis must be thorough, diverse, and insightful enough to be useful for the data science team. "
             "Generate at least a few insightful visualizations with a brief explanation of what they show. "
         )
+
         files = [
             File.from_path(filepath),
         ]
@@ -30,4 +31,44 @@ async def analyze_file(filepath: str) -> tuple[str, list[File]]:
         # for file in response.files:
         #     file.show_image()
 
-        return response.content, response.files
+        return response
+
+
+async def analyze_ml_models(filepath: str) -> list[CodeInterpreterResponse]:
+    async with CodeInterpreterSession() as session:
+        # define the user request
+        user_request = (
+            "Help me build, train, and evaluate machine learning models for this dataset. "
+            "The models must be able to predict the target variable with high accuracy. "
+            "You are a great data scientist, so you can use any model you want. "
+            "You can use any machine learning algorithm and library you want. "
+            "Continue to improve the models until you are satisfied with the results. "
+            "Feel free to iterate through different models and hyperparameters. "
+            "You need to answer step-by-step how you built the models and why you chose them. "
+            "You are given memory and you therefore do not need to worry about solving the problem in one go. "
+            "Only once you are satisfied with the results, return only '<DONE>' as the answer."
+            "However, you are only given {STEPS} steps to solve this problem. This is step {STEP}."
+        )
+
+        files = [
+            File.from_path(filepath),
+        ]
+
+        responses = []
+        response = ""
+        max_steps = 12
+        step = 0
+        while "<DONE>" not in response and step < max_steps:
+            step += 1
+            # generate the response
+            response = await session.agenerate_response(
+                user_request.format(STEPS=max_steps, STEP=step), files=files
+            )
+            responses.append(response)
+
+            # Output to the user
+            # print("AI: ", response.content)
+            # for file in response.files:
+            #     file.show_image()
+
+        return responses
--- routers/analyze.py
+++ routers/analyze.py
@@ -8,18 +8,15 @@ import matplotlib.pyplot as plt
 from fastapi import FastAPI, APIRouter, Depends
 
 from dependencies import get_user_id
-from mlbot.agent import analyze_file
+from mlbot.agent import analyze_file, analyze_ml_models
 
 app = FastAPI()
 
 router = APIRouter(prefix="/analyze", tags=["analyze"])
 
 
-def plt_to_base64():
-    buf = io.BytesIO()
-    plt.savefig(buf, format="png")
-    plt.close()
-    return base64.b64encode(buf.getvalue()).decode("utf-8")
+def image_data_to_base64(image_data: bytes) -> str:
+    return base64.b64encode(image_data).decode("utf-8")
 
 
 @router.get("/{file_id}")
@@ -28,7 +25,42 @@ async def _analyze_file(
     user_id: UUID = Depends(get_user_id),
 ):
     response = await analyze_file(f"data/users/{user_id}/{file_id}.parquet")
+
+    # List to store Base64 encoded images
+    encoded_images = []
+
+    for file in response.files:
+        image_data = (
+            file.get_data()
+        )  # This assumes that there's a method to get image data in bytes
+        encoded_images.append(image_data_to_base64(image_data))
+
     return {
-        "answer": response[0],
-        "files": [{"image_base64": plt_to_base64()} for _ in response[1]],
+        "answer": response.content,
+        "files": encoded_images,
     }
+
+
+@router.get("/models/{file_id}")
+async def _analyze_ml_models(
+    file_id: UUID,
+    user_id: UUID = Depends(get_user_id),
+):
+    responses = await analyze_ml_models(f"data/users/{user_id}/{file_id}.parquet")
+
+    vals = []
+    for response in responses:
+        # List to store Base64 encoded images
+        encoded_images = []
+        for file in response.files:
+            image_data = file.get_data()
+            encoded_images.append(image_data_to_base64(image_data))
+
+        vals.append(
+            {
+                "answer": response.content,
+                "files": encoded_images,
+            }
+        )
+
+    return vals
